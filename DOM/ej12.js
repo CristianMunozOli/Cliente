5ece9526f37f2df9c3606236378df68cbdfb020e
cmo_d <230183@fppiramide.com>
2023-12-03 20:33:00 +0100

    No crea bien los hijos de la sublista

--- DOM/ej12.js
+++ DOM/ej12.js
@@ -22,16 +22,25 @@ function crearBotonAnadir(contenedor) {
     const botonAnadirTarea = document.createElement('button');
     botonAnadirTarea.textContent = "Añadir tarea";
     botonAnadirTarea.addEventListener('mousedown',() =>{
-        const nombreTarea = prompt('Introduce el nombre de la tarea:');
-        const nuevaTarea = document.createElement('li');
-        nuevaTarea.textContent = nombreTarea;
-        const ul = contenedor.querySelector('ul');
-        ul.append(nuevaTarea);
+        
+        //Crear botones para añadir antes, añadir después y borrar la tarea
+           crearBotones(contenedor);
+        
+       /* const botonAnadirTareaDespues = document.createElement('button');
+        botonAnadirTareaDespues.textContent = "Añadir Despues";
+        botonAnadirTareaDespues.addEventListener('mousedown',() =>{
+            
+        });
+        const botonBorrarTarea = document.createElement('button');
+        botonBorrarTarea.textContent = "Borrar";
+        botonBorrarTarea.addEventListener('mousedown',() =>{
+            
+        });*/
+       
     });
     const div =contenedor;
     div.append(botonAnadirTarea);
 }
-
 function borrarLista(lista) {
     const botonBorrar = document.createElement('button');
     botonBorrar.textContent = "Borrar Lista";
@@ -41,4 +50,27 @@ function borrarLista(lista) {
         lista.remove();
         botonBorrar.remove();
     });
+}
+function crearBotones(contenedor) {
+    const nombreTarea = prompt('Introduce el nombre de la tarea:');
+    const nuevaTarea = document.createElement('li');
+    nuevaTarea.textContent = nombreTarea;
+    const botonAnadirTareaAntes = document.createElement('button');
+        botonAnadirTareaAntes.textContent = "Añadir Antes";
+        botonAnadirTareaAntes.addEventListener('mousedown',() =>{
+            crearBotones(contenedor);
+        });  
+        const siguienteElemento = nuevaTarea.nextSibling;
+        const ul = contenedor.querySelector('ul');
+         // Insertar la nueva tarea después del elemento siguiente (o al final si es null)
+         if (siguienteElemento) {
+            ul.insertBefore(nuevaTarea, siguienteElemento);
+            ul.append(botonAnadirTareaAntes); 
+        } else {
+            ul.append(nuevaTarea);
+            ul.append(botonAnadirTareaAntes); 
+        }
+       
+       // ul.append(nuevaTarea);
+       // ul.append(botonAnadirTareaAntes);     
 }
