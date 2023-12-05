2ea0a41e7d9868fac1e05abc2711c6c62e35a6a7
Cristian <230183@fppiramide.com>
2023-12-05 10:18:01 +0100

    5/12

--- DOM/ej12.js
+++ DOM/ej12.js
@@ -21,24 +21,24 @@ function anadeLista() {
 function crearBotonAnadirTarea(contenedor) {
     const botonAnadirTarea = document.createElement('button');
     botonAnadirTarea.textContent = "Añadir tarea";
-    
     botonAnadirTarea.addEventListener('mousedown',(e) =>{
-        
-        //Crear botones para añadir antes, añadir después y borrar la tarea
-           crearBotones(e);
-        
-       /* const botonAnadirTareaDespues = document.createElement('button');
+        const nombreTarea = prompt('Introduce el nombre de la tarea:');
+        const nuevaTarea = document.createElement('li');
+        nuevaTarea.textContent = nombreTarea; 
+        const botonAnadirTareaAntes = document.createElement('button');
+        botonAnadirTareaAntes.textContent = "Añadir Antes";
+        botonAnadirTareaAntes.addEventListener('mousedown',anadirAntes);  
+        const botonAnadirTareaDespues = document.createElement('button');
         botonAnadirTareaDespues.textContent = "Añadir Despues";
-        botonAnadirTareaDespues.addEventListener('mousedown',() =>{
-            
-        });
+        botonAnadirTareaDespues.addEventListener('mousedown',anadirDespues);
         const botonBorrarTarea = document.createElement('button');
         botonBorrarTarea.textContent = "Borrar";
-        botonBorrarTarea.addEventListener('mousedown',() =>{
-            
-        });*/
-       
+        botonBorrarTarea.addEventListener('mousedown',(e)=>e.target.parentElement.remove());
+        e.target.parentNode.append(nuevaTarea);
+        nuevaTarea.append(botonBorrarTarea,botonAnadirTareaAntes,botonAnadirTareaDespues)
+    
     });
+   
     const div = document.querySelectorAll('div');
     div[div.length-1].append(botonAnadirTarea);
 }
@@ -53,29 +53,43 @@ function crearBorrarBotonLista() {
     });
 }
 
-function crearBotones(contenedor) {
-    const nombreTarea = prompt('Introduce el nombre de la tarea:');
-    const nuevaTarea = document.createElement('li');
-    nuevaTarea.textContent = nombreTarea;
-    const botonAnadirTareaAntes = document.createElement('button');
-    botonAnadirTareaAntes.textContent = "Añadir Antes";
-    botonAnadirTareaAntes.addEventListener('mousedown',() =>{
-        crearBotones(contenedor);
-        });   
-    
-    const siguienteElemento = nuevaTarea.nextSibling;
-    const ul = contenedor.querySelector('ul');
-    console.log(siguienteElemento)
-    console.log(nuevaTarea)
-    if (siguienteElemento) {
-        //console.log("1");
-        ul.insertBefore(nuevaTarea, siguienteElemento);
-        ul.append(botonAnadirTareaAntes); 
-    } else {
-        //console.log("2");
-        ul.append(nuevaTarea);
-        ul.append(botonAnadirTareaAntes); 
-    }
-    // ul.append(nuevaTarea);
-    // ul.append(botonAnadirTareaAntes);     
+function anadirAntes(e){
+    const nombreTarea=prompt("Nombre de la tarea");
+    const tareaNueva=document.createElement("li");
+    tareaNueva.innerText=nombreTarea;
+    //Boton Eliminar Tarea
+    const btnEliminarTarea=document.createElement("button",);
+    btnEliminarTarea.innerText="Eliminar Tarea";
+    btnEliminarTarea.addEventListener("click",(e)=>e.target.parentElement.remove());
+    //Boton Tarea antes
+    const btnTareaAntes=document.createElement("button",);
+    btnTareaAntes.innerText="Añadir Tarea Antes";
+    btnTareaAntes.addEventListener("click",anadirAntes);
+    //Boton Tarea Despues
+    const btnTareaDespues=document.createElement("button",);
+    btnTareaDespues.innerText="Añadir Tarea Despues";
+    btnTareaDespues.addEventListener("click",anadirDespues);
+    e.target.parentNode.before(tareaNueva);
+    //Meto los botones dentro del li, para Drag and drop creo que ira mejor
+    tareaNueva.append(btnEliminarTarea,btnTareaAntes,btnTareaDespues)
+}
+function anadirDespues(e){
+    const nombreTarea=prompt("Nombre de la tarea");
+    const tareaNueva=document.createElement("li");
+    tareaNueva.innerText=nombreTarea;
+    //Boton Eliminar Tarea
+    const btnEliminarTarea=document.createElement("button",);
+    btnEliminarTarea.innerText="Eliminar Tarea";
+    btnEliminarTarea.addEventListener("click",(e)=>e.target.parentElement.remove());
+    //Boton Tarea antes
+    const btnTareaAntes=document.createElement("button",);
+    btnTareaAntes.innerText="Añadir Tarea Antes";
+    btnTareaAntes.addEventListener("click",añadirAntes);
+    //Boton Tarea Despues
+    const btnTareaDespues=document.createElement("button",);
+    btnTareaDespues.innerText="Añadir Tarea Despues";
+    btnTareaDespues.addEventListener("click",añadirDespues);
+    e.target.parentNode.after(tareaNueva);
+    //Meto los botones dentro del li, para Drag and drop creo que ira mejor
+    tareaNueva.append(btnEliminarTarea,btnTareaAntes,btnTareaDespues)
 }
